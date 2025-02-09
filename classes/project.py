a657582006c68c9c677bb633b08136bd452bf69e
Lars Kruse <lakr@thetechcollective.eu>
2025-02-09 09:39:43 +0000

    added the Project class and support for updating fields in projects

--- classes/project.py
+++ classes/project.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import sys
 import re
+import json
 
 # Add directory of this class to the general class_path
 # to allow import of sibling classes
@@ -14,16 +15,16 @@ class Project:
     """Class used to represent the Devbranch for a development contribution"""
 
     props = {
-        'project_owner': 'lakruzz',
-        'project_number': '12',
+        'project_owner': 'None',
+        'project_number': 'None',
+        'verbose': False,
+        'workdir': None,
         'project_id': None,
         'item_id': None,
         'field_id': None,
         'field_type': None,
         'option_id': None,
-        'issue_url': None,
-        'verbose': False,
-        'workdir': None,
+        'issue_url': None
     }
 
     props['workdir'] = os.getcwd()
@@ -31,29 +32,189 @@ class Project:
   
     def verbose(self, verbose):
         self.props['verbose'] = verbose
-        
-    def verbose_print(self, message):
-        if self.props['verbose'] == True:
-            print(message)
             
 
     # Instance methods
     
     def __init__(self, owner=str, number=int, workdir=None, verbose=False):
-        self.props['verbose'] = verbose
         if workdir:
             # check if the directory exists
             if not os.path.exists(workdir):
                 raise FileNotFoundError(f"Directory {workdir} does not exist")
                 sys.exit(1)
             self.props['workdir'] = os.path.abspath(workdir)
-        self.__read_project()
             
-    def __read_project(self):
-        """Desinged to be called from __init__ to read the project context in the repo"""
-        ## get the project id
-        [self.props['project_id'], result] = run(
-            f"gh project view --owner {self.props['project_owner']} {self.props['project_number']} --format json --jq '.id'",
-            msg="Get the project id")
+        self.props['project_owner'] = owner
+        self.props['project_number'] = number
+        self.props['verbose'] = verbose
+ 
+        
+    def get_item_id_from_url(self, owner=None, number=None, url=str):
+        """Get the project item id from the issue url (consider using add_issue(url) to get the item-id instead)
+        
+        Args:
+            owner (str): The owner of the project (optional)
+            number (int): The project number (optional)
+            url (str): The url of the issue
+        Returns:
+            item_id (str): The item id of the issue if it is added to the project
+            None: If the issue is not added to the project
+        """
+        
+        if not owner:
+            owner = self.props['project_owner']
+        if not number:
+            number = self.props['project_number']
+        
+        [item_id, result] = run(
+            f"gh project item-list {number} --owner {owner} --limit 1000 --format json --jq '.items[] | select(.content.url == \"{url}\") | .id'",
+            verbose=self.props['verbose'],
+            msg="Get the item id from the url")
+        return item_id
+    
+    def get_url_from_issue(self, issue=int):
+        """Get the url of the issue in context of the current repository
+        
+        Args:
+            issue (int): The issue number
+        Returns: 
+            url (str): The url of the issue
+        Raises:
+            RuntimeError: If the issue number is not found
+        """
+        [url, result] = run(
+            f"gh issue view {issue} --json url --jq '.url'",
+            verbose=self.props['verbose'],
+            msg="Get the url from the issue")
+        return url
+    
+    def get_project_id(self, owner=None, number=None):
+        """Get the project id from the project owner and number
+        
+        Args:
+            owner (str): The owner of the project (optional)
+            number (int): The project number (optional)
+        Returns:
+            id (str): The project id
+        Raises:
+            RuntimeError: If the project owner or number is not
+        """
+        if not owner:
+            owner = self.props['project_owner']
+        if not number:
+            number = self.props['project_number']
+            
+        [id, result] = run(
+            f"gh project view {number} --owner {owner} --format json --jq '.id'",
+            verbose=self.props['verbose'],
+            msg="Get the project id")      
+        return id   
+    
+    def get_field_description(self, owner=None, number=None, field=str):
+        """Get the field descriptions from the project
+        
+        Args:   
+            owner (str): The owner of the project (optional)
+            number (int): The project number (optional)
+            field (str): The field name 
+        Returns:
+            field_json (dict): The field description in json format
+        Raises:
+            RuntimeError: If the field is not found
+        """
+        
+        if not owner:
+            owner = self.props['project_owner']
+        if not number:
+            number = self.props['project_number']
+                    
+        [field_json_str, result] = run(
+            f"gh project field-list {number} --owner {owner} --format json --jq '.fields[] | select(.name == \"{field}\")'",
+            verbose=self.props['verbose'],
+            msg="Get the field description in json format")
+        field_json = json.loads(field_json_str)
+        if field_json_str == '':
+            raise RuntimeError(f"Field {field} not found in project {owner}/{number}\n{result.stderr}")
+            sys.exit(1)
+        return field_json
+    
+    def update_field(self, owner=str, number=int, url=str, field=str, field_value=str):
+        """Update the field with the value
+        
+        Args:
+            owner (str): The owner of the project (optional)
+            number (int): The project number (optional)
+            url (str): The url of the issue
+            field (str): The field name
+            field_value (str): The field value
+        Returns:
+            result (str): The result of the update
+        """
+        if not owner:
+            owner = self.props['project_owner']
+        if not number:
+            number = self.props['project_number']
+
+        project_id = self.get_project_id()
+        item_id = self.add_issue(url=url)
+        field_desc = self.get_field_description(field=field)
+
+        # read the field description        
+        field_id = field_desc['id']
+        field_type= field_desc['type']
+        field_option_id = None
+        for option in field_desc['options']:
+            if option['name'] == field_value:
+                field_option_id = option['id']  
+                
+        if field_id == None or field_option_id == None:
+            raise RuntimeError(f"Field {field} not found in project {owner}/{number}")
+            sys.exit(1)
+        
+        if field_option_id == None:
+            raise RuntimeError(f"Field option value {field_value} not found in field {field}")
+            sys.exit(1)
+          
+        type_to_switch_conversion = {
+            'ProjectV2SingleSelectField': '--single-select-option-id',
+            'ProjectV2Field': '--text'
+            }
+                     
+        type_switch = type_to_switch_conversion[field_type]     
+        if type_switch == None:
+            raise RuntimeError(f"Field type {field_type} not supported")
+            sys.exit(1)
+             
+        [output, result] = run(
+            f"gh project item-edit --project-id {project_id} --field-id {field_id} --id {item_id} {type_switch} {field_option_id}",
+            verbose=self.props['verbose'],
+            msg="Update the field with the option")    
+        
+        return output
+    
+    def add_issue(self, owner=None, number=None, url=str):
+        """Add an issue to the project. This function is idempotent, It doesn't affect the
+        project if the issue is already added to the project.
+        
+        Args:
+            owner (str): The owner of the project (optional)
+            number (int): The project number (optional)
+            url (str): The url of the issue
+        Returns:
+            result (str): item-id of the issue in the project
+        """
+        if not owner:
+            owner = self.props['project_owner']
+        if not number:
+            number = self.props['project_number']
+            
+        [id, result] = run(
+            f"gh project item-add {number} --owner {owner} --url {url} --format json --jq '.id'",
+            verbose=self.props['verbose'],
+            msg="Add the issue to the project")
+        return id
+         
+            
+            
 
 
