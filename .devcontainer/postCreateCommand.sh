352a5d04f5ed481fbcc5b62c7b0faffd1abf06d1
Lars Kruse <lakr@thetechcollective.eu>
2025-02-15 11:13:30 +0000

    Enabled the 'gh note' feature, using gh aliases and devcontainer setup - fixed #18

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
+PREFIX="$(basename $0): "
+
 pipenv sync --dev
 git config --local --get include.path | grep -e ../.gitconfig || git config --local --add include.path ../.gitconfig
+
+$(dirname $0)/gh-login.sh postcreate
+gh extension install thetechcollective/gh-tt
+gh alias import .gh-alias.yml
