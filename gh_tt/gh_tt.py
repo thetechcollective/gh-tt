2ad93bcf741c0854235f89f7a0b8a2482c05f697
Tomas Vemola <tomas@tomasvemola.com>
2026-02-10 11:05:58 +0000

    basic logger

--- gh_tt/gh_tt.py
+++ gh_tt/gh_tt.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
-
+import logging
 import sys
+import typing
 
 from gh_tt.classes.gitter import Gitter
 from gh_tt.modules.tt_handlers import COMMAND_HANDLERS
 from gh_tt.modules.tt_parser import tt_parse
 
 
+def setup_logging(verbose: int):
+    match verbose:
+        case 2:
+            level = logging.DEBUG
+        case 1:
+            level = logging.INFO
+        case int():
+            level = logging.WARNING
+        case _:
+            typing.assert_never()
+
+    logging.basicConfig(
+        level=level,
+        stream=sys.stderr,
+    )
+
+
 def main():
     args = tt_parse(sys.argv[1:])
 
-    Gitter.set_verbose(value=args.verbose)
+    setup_logging(args.verbose)
+
+    legacy_gitter_verbose = args.verbose >= 1
+
+    Gitter.set_verbose(value=legacy_gitter_verbose)
     Gitter.validate_gh_version()
 
     if args.version:
